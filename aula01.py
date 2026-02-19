2b150dd60920b143e9039a766bfd273f41fa7d3a
lara kelman de oliveira <lara.oliveira01097483@sesisenaipr.org.br>
2026-02-19 10:08:01 -0300

    primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 15 # variável inteira 
+peso = 61 # variável float 
+nome = "Lara kelman de oliveira" # variável string 
+sexo_feminino = True # variável bool ,,
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino) 
