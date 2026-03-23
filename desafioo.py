f373028ec4d41040d8099d9d9dc22f1e0fd170e7
lara <lara.oliveira01097483@sesisenaipr.org.br>
2026-03-23 12:43:09 -0300

    aula dia 23

---
+++ desafioo.py
@@ -0,0 +1,4 @@
+autorizacao = ["alice" , "bob" , "carlos"] 
+usuario = input("digite o nome de um pesquisador: ") 
+
+if 
--- pop.py
+++ pop.py
@@ -1,2 +1,6 @@
 lista_acoes = ["digitar texto" , "muda cor" , "inserir imagem"]
-acoes.remove 
+lista_acoes.remove
+lista_acoes.pop(2)
+
+lista = len (lista_acoes)
+print(f"lista atual{lista_acoes}")
