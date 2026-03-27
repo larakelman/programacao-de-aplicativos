1b9c08a888dcd8e1b860ae07563881eab9890f98
lara <lara.oliveira01097483@sesisenaipr.org.br>
2026-03-27 10:08:52 -0300

    aula 27

---
+++ esvaziando.py
@@ -0,0 +1,5 @@
+fila = ["ana", "carlos" , "joao" , "maria" , "pedro"] 
+
+while len(fila) > 0:
+    atendido = fila.pop(0) 
+    print(atendido, "foi atendido") 
---
+++ temperatura.py
@@ -0,0 +1,7 @@
+temps = [28.5, 31.0, 29.8, 33.5, 27.0, 35.2, 30.0]
+ 
+for valor in temps:
+    if valor >30.0 
+        print(f"ALERTA: temperatura critica! ({valor}°C)")
+    else: 
+        print(f"temperatura normal. ({valor}°C)")
